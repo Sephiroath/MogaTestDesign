7bed027be375dcd380f78e5a17a7c95cdeda0464
caalbarraicnm@gmail.com <Andres1230>
2018-07-09 20:31:07 -0500

    first version menu choices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  navigation = [
+    { link: 'pasttrials', label: 'Past Trials' },
+    { link: 'howitworks', label: 'How It Works' },
+    { link: 'login', label: 'Log in/Sign Up' }
+  ];
 }
