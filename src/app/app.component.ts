820f03981432b9c8344992ba31c84f9da6649026
caalbarraicnm@gmail.com <Andres1230>
2018-07-11 14:53:53 -0500

    speak out first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  shouldDisplayMenuMobile = false;
   navigation = [
     { link: 'pasttrials', label: 'Past Trials' },
     { link: 'howitworks', label: 'How It Works' },
     { link: 'login', label: 'Log in/Sign Up' }
   ];
+  displayMenuMobile() {
+    this.shouldDisplayMenuMobile = !this.shouldDisplayMenuMobile;
+  }
 }
