a171117b19960c6b0ee57dcae4c9b3961eae5a95
caalbarraicnm@gmail.com <Andres1230>
2018-07-14 13:20:51 -0500

    new navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  shouldDisplayMenuMobile = false;
+  shouldShowNavBar = false;
   navigation = [
     { link: 'pasttrials', label: 'Past Trials' },
     { link: 'howitworks', label: 'How It Works' },
     { link: 'login', label: 'Log in/Sign Up' }
   ];
-  displayMenuMobile() {
-    this.shouldDisplayMenuMobile = !this.shouldDisplayMenuMobile;
-  }
 }
